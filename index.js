e590849e05900c51b109ee93663345640dbf49e1
gabygaby2007 <127844758+gabygaby2007@users.noreply.github.com>
2024-09-12 02:54:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+ const {time} = require('console');
+
+let anoAtual = 2024;
+let idade = (16);
+const nomeCompleto = "Gabrielly Jaros";
+const faculdade = "falculdade medicina";
+let cidadeAtual = "SJP";
+let lazer = "jogar";
+
+console.log(olá eu sou o ${nomeCompleto}, tenho ${idade} anos, moro em ${cidadeAtual} e no ano que vem espero estar cursando ${faculdade});
+
+anoAtual = 2025;
+idade = 16;
+cidadeAtual = "SJP";
+depoisMarço = "faculdade medicina";
+lazer = "jogar"
+
+console.log(Para o ano de ${anoAtual} espero ja possuir meus bens materiais que anseio e estar cursando e estudando a minha área de desejo);
