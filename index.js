202ba2004b06a3618165e74307ffc0bce4656097
gabygaby2007 <127844758+gabygaby2007@users.noreply.github.com>
2024-09-19 01:37:54 +0000

    18/09

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const faculdade = "falculdade medicina";
 let cidadeAtual = "SJP";
 let lazer = "jogar";
 
-console.log(olá eu sou o ${nomeCompleto}, tenho ${idade} anos, moro em ${cidadeAtual} e no ano que vem espero estar cursando ${faculdade});
+console.log(`olá eu sou o ${nomeCompleto}, tenho ${idade} anos, moro em ${cidadeAtual} e no ano que vem espero estar cursando ${faculdade}`);
 
 anoAtual = 2025;
 idade = 16;
@@ -15,4 +15,27 @@ cidadeAtual = "SJP";
 depoisMarço = "faculdade medicina";
 lazer = "jogar"
 
-console.log(Para o ano de ${anoAtual} espero ja possuir meus bens materiais que anseio e estar cursando e estudando a minha área de desejo);
+console.log(`Para o ano de ${anoAtual} espero ja possuir meus bens materiais que anseio e estar cursando e estudando a minha área de desejo`);
+
+//Agencia de viagens
+
+const nomeComprador = "Ricardo";
+const cpfComprador = 13565219899;
+const idadeComprador = 65;
+const ListaDeDestinos= new Array(
+"Rio de Janeiro",
+"São Paulo",
+"Brasilia",
+"Belo Horizonte",
+"Salvador."
+);
+
+console.log(`Olá senhor ${nomeComprador}, seu cpf é ${cpfComprador} e sua idade é ${idadeComprador} anos, e você deseja viajar para ${ListaDeDestinos}`);
+
+console.log(ListaDeDestinos[2]);
+console.log("");
+console.log("Olá senhor $(nomeComprador) adicionamos mais uma viajem a sua lista de interesses.");
+
+ListaDeDestinos.push("Brasilia");
+
+console.log(ListaDeDestinos);
